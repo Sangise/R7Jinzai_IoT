ed239780df5f8c3b8e3fda1314966a4ee06409ae
Sangise <86819422+Sangise@users.noreply.github.com>
2025-09-22 14:45:01 +0900

    Add files via upload

---
+++ 第3回/page40.ino
@@ -0,0 +1,36 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+// ---- 書き換えポイント ----
+const char* WIFI_SSID = "ここにwifiのSSID";
+const char* WIFI_PASS = "ここにwifiのパスワード";
+const char* GAS_URL  = "ここにGASのデプロイのURL";
+
+void setup() {
+  pinMode(34, ANALOG);
+
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) ;
+  Serial.println("WiFi connected.");
+}
+
+void loop() {
+  int data1 = analogRead(34);
+  int data2 = 400;
+
+  String url = String(GAS_URL) + "?data1=" + data1 + "&data2=" + data2;
+  Serial.println(url);
+
+  WiFiClientSecure client;
+  client.setInsecure();
+  
+  HTTPClient http;
+  http.begin(client, url);
+  http.GET();
+  http.end();
+
+  delay(30000); // 30秒ごと
+}
